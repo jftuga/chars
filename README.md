1fc7a8107097874d2dd86d3c170b10ba5daa9960
John Taylor <johntaylor@gmail.com>
2022-01-17 05:50:00 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # chars
 Determine the end-of-line format, tabs, bom, and nul characters
+* Pass (multiple) filename wildcards on the command line
+* use `-b` to also examine binary files
+
+## Download
+
+Binaries for Windows, MacOS, Linux and FreeBSD are provided on the
+[releases page](https://github.com/jftuga/chars/releases).
+
+## Usage
+
+```shell
+chars v1.1.0
+Determine the end-of-line format, tabs, bom, and nul
+https://github.com/jftuga/chars
+
+Usage:
+chars [file-glob 1] [file-glob 2] ...
+  -b    examine binary files
+
+Notes:
+Also try: chars *  -or-  chars */*  -or-  chars */*/*
+```
+
+## Example
+
+```shell
+PS C:\chars> .\chars.exe *
+
++-----------------+------+------+------+--------+------+-------+
+|    FILENAME     | CRLF |  LF  | TAB  |  NUL   | BOM8 | BOM16 |
++-----------------+------+------+------+--------+------+-------+
+| .gitignore      |    0 |   16 |    0 |      0 |    0 |     0 |
+| .goreleaser.yml |    0 |   59 |    0 |      0 |    0 |     0 |
+| LICENSE         |    0 |   21 |    0 |      0 |    0 |     0 |
+| README.md       |    0 |   23 |    0 |      0 |    0 |     0 |
+| cmd.go          |    0 |  148 |  148 |      0 |    0 |     0 |
+| go.mod          |    0 |    7 |    0 |      0 |    0 |     0 |
+| go.sum          |    0 |    4 |    0 |      0 |    0 |     0 |
+| testfile1       |   19 |   19 |    0 |      0 |    0 |     0 |
+| testfile2       |    0 |   21 |    0 |   2269 |    0 |     1 |
++-----------------+------+------+------+--------+------+-------+
+```
---
+++ cmd.go
@@ -0,0 +1,202 @@
+/*
+chars
+-John Taylor
+Jan-16-2022
+
+Determine the end-of-line format, tabs, bom, and nul
+Pass wildcard filename globs on the command line
+*/
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"unicode/utf8"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+const pgmName string = "chars"
+const pgmDesc string = "Determine the end-of-line format, tabs, bom, and nul"
+const pgmUrl string = "https://github.com/jftuga/chars"
+const pgmVersion string = "1.1.0"
+
+type FileStat struct {
+	filename string
+	crlf     string
+	lf       string
+	tab      string
+	bom8     string
+	bom16    string
+	nul      string
+}
+
+// usage - display help when no cmd-line args given
+func usage() {
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "%s v%s\n", pgmName, pgmVersion)
+	fmt.Fprintln(os.Stderr, pgmDesc)
+	fmt.Fprintln(os.Stderr, pgmUrl)
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintln(os.Stderr, "Usage:")
+	fmt.Fprintf(os.Stderr, "%s [file-glob 1] [file-glob 2] ...\n", pgmName)
+	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintln(os.Stderr, "Notes:")
+	fmt.Fprintf(os.Stderr, "Also try: %s *  -or-  %s */*  -or-  %s */*/*\n", pgmName, pgmName, pgmName)
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+// isText - if 2% of the bytes the first 1024 bytes are non-printable,
+// then consider the file to be a binary file
+func isText(s []byte) bool {
+	const max = 1024 // at least utf8.UTFMax
+	if len(s) > max {
+		s = s[0:max]
+	}
+
+	bin := 0
+	for i, c := range string(s) {
+		if i <= 3 {
+			// skip bom characters
+			continue
+		}
+		if i+utf8.UTFMax > len(s) {
+			// last char may be incomplete - ignore
+			break
+		}
+		if c == 0xFFFD || c < ' ' && c != '\n' && c != '\t' && c != '\f' && c != '\r' && c != 0x00 {
+			// decoding error or control character - not a text file
+			bin += 1
+		}
+	}
+	numerator := float32(bin) / float32(len(s))
+	if numerator > 0.02 {
+		return false
+	}
+	return true
+}
+
+// getFile - return the contents of a file in a []byte slice
+func getFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	stats, statsErr := file.Stat()
+	if statsErr != nil {
+		return nil, statsErr
+	}
+
+	var size int64 = stats.Size()
+	bytes := make([]byte, size)
+
+	buf := bufio.NewReader(file)
+	_, err = buf.Read(bytes)
+
+	return bytes, err
+}
+
+// detect - tabulate the total number of special characters
+// in the given []byte slice
+func detect(filename string, data []byte) FileStat {
+	position := 0 // file position
+	tab := 0      // 9
+	lf := 0       // 10
+	crlf := 0     // 13
+	bom8 := 0     // 239,187,191
+	bom16 := 0    // 255,254
+	nul := 0      // 0
+	for i, b := range data {
+		if b == 10 {
+			lf += 1
+		} else if b == 9 {
+			tab += 1
+		} else if b == 0 {
+			nul += 1
+		}
+		if position >= 1 {
+			if b == 10 && data[i-1] == 13 {
+				crlf += 1
+			}
+		}
+		position += 1
+	}
+
+	if len(data) >= 3 {
+		if data[0] == 239 && data[1] == 187 && data[2] == 191 {
+			bom8 += 1
+		} else if data[0] == 255 && data[1] == 254 {
+			bom16 += 1 // little-endian
+		} else if data[0] == 254 && data[1] == 255 {
+			bom16 += 1 // big-endian
+		}
+	}
+
+	return FileStat{filename: filename, crlf: strconv.Itoa(crlf), lf: strconv.Itoa(lf), tab: strconv.Itoa(tab), bom8: strconv.Itoa(bom8), bom16: strconv.Itoa(bom16), nul: strconv.Itoa(nul)}
+}
+
+// output - display a text table with each filename and the number of special characters
+func output(allStats []FileStat) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"filename", "crlf", "lf", "tab", "nul", "bom8", "bom16"})
+	for _, s := range allStats {
+		row := []string{s.filename, s.crlf, s.lf, s.tab, s.nul, s.bom8, s.bom16}
+		table.Append(row)
+	}
+	table.Render()
+}
+
+// processGlob - process of files contained within a single cmd-line arg glob
+func processGlob(globArg string, allStats *[]FileStat, examineBinary bool) {
+	globFiles, err1 := filepath.Glob(globArg)
+	if err1 != nil {
+		panic(err1)
+	}
+	for _, filename := range globFiles {
+		//fmt.Println(filename)
+		info, _ := os.Stat(filename)
+		if info.IsDir() {
+			continue
+		}
+		data, err2 := getFile(filename)
+		if !examineBinary {
+			if !isText(data) {
+				//fmt.Printf("binary file: %s\n", filename)
+				continue
+			}
+		}
+		if err2 != nil {
+			fmt.Fprintf(os.Stderr, "Unable to process: %s\n", filename)
+			continue
+		}
+		stats := detect(filename, data)
+		*allStats = append(*allStats, stats)
+	}
+}
+
+func main() {
+	argsBinary := flag.Bool("b", false, "examine binary files")
+	flag.Usage = usage
+	flag.Parse()
+	allGlobs := flag.Args()
+	if len(allGlobs) == 0 {
+		usage()
+		return
+	}
+
+	var allStats []FileStat
+	for _, globArg := range allGlobs {
+		processGlob(globArg, &allStats, *argsBinary)
+	}
+
+	output(allStats)
+}
